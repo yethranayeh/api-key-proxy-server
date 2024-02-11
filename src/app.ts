04f57561218ea4331f4e9715710b65e5d6574bc4
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2024-02-11 10:03:57 +0300

    add "type" import distinction

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 import Debug from 'debug';
 import express from 'express';
 import { resolve } from 'path';
